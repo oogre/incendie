68fb8b4345446827ceef69cdbd206d96cb7d98ba
Matière humaine <matierehumaine@matiere.local>
2025-01-07 16:02:36 +0100

    sounds

---
+++ server.indendie/src/Sounds/index.js
@@ -0,0 +1,37 @@
+import UDP from 'dgram';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const server = UDP.createSocket('udp4')
+
+
+const IP = process.env.SOUND_IP;
+const PORT = parseInt(process.env.SOUND_OUT_PORT);
+const INPORT = parseInt(process.env.SOUND_IN_PORT);
+
+
+server.bind(INPORT, ()=>{
+	server.setBroadcast(false);	
+});
+
+const BULBS = async () => {
+	let promise;
+	return {
+		send : async (buffer)=>{
+			if(promise != undefined){
+				console.log("blocked");
+				return;
+			}
+  			promise = new Promise ((resolve, reject)=>{
+					server.send(buffer, 0, buffer.length, PORT, IP, function(err) {
+		        		promise = undefined;
+						if (err) reject(err);
+						else resolve();
+		    		});
+  			});
+  			return promise;
+		}
+	}
+}
+
+export default BULBS();
